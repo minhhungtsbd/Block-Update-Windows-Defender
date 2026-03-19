bf58a2ef524b19e62852ed301e3e7603cd4f3948
Minh Hưng <minhhungtsbd@gmail.com>
2026-03-19 09:56:40 +0700

    Release v1.1.4: keep English title and add software update repo info

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,6 @@
+using System.Diagnostics;
 using System.Windows;
+using System.Windows.Navigation;
 using BlockUpdateWindowsDefender.ViewModels;
 
 namespace BlockUpdateWindowsDefender
@@ -17,6 +19,22 @@ namespace BlockUpdateWindowsDefender
         private async void Window_Loaded(object sender, RoutedEventArgs e)
         {
             await _viewModel.InitializeAsync();
+        }
+
+        private void RepositoryLink_RequestNavigate(object sender, RequestNavigateEventArgs e)
+        {
+            try
+            {
+                Process.Start(new ProcessStartInfo
+                {
+                    FileName = e.Uri.AbsoluteUri,
+                    UseShellExecute = true
+                });
+                e.Handled = true;
+            }
+            catch
+            {
+            }
         }
     }
 }
